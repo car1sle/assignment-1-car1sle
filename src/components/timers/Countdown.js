f3423ff6b388d23a1507433070a07a5321c9c6d7
Carly Ciricillo <carly.ciricillo@wunderkind.co>
2022-10-20 22:57:18 -0400

    First two timers functionality done

--- src/components/timers/Countdown.js
+++ src/components/timers/Countdown.js
@@ -1,3 +1,67 @@
-const Countdown = () => null;
+import { useEffect, useState } from 'react';
+import Button from '../generic/Button';
+
+const Countdown = () => {
+
+    const [time, setTime] = useState(0);
+    const [inputTime, setInputTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [isComplete, setIsComplete] = useState(true);
+    const [inputIsDisabled, setInputIsDisabled] = useState(false);
+
+    const handleClick = value => {
+        if (value === "Start") {
+            setIsRunning(true);
+            setIsComplete(false);
+            setInputIsDisabled(true);
+        } else if (value === "Pause") {
+            setIsRunning(false);
+        } else if (value === "Resume") {
+            setIsRunning(true);
+        } else if (value === 'Fast Forward') {
+            setTime(0);
+            setIsComplete(true);
+            setInputIsDisabled(false);
+        } else if (value === 'Reset') {
+            setTime(inputTime);
+            setIsComplete(true);
+            setIsRunning(false);
+            setInputIsDisabled(false);
+        }
+    }
+
+    useEffect(() => {
+
+        let i;
+
+        if (isRunning) {
+            i = setInterval(() => {
+                setTime(time - 1);
+            }, 1000);
+            if (time === 0) {
+                clearInterval(i);
+                setIsRunning(false);
+            }
+        }
+
+        return () => clearInterval(i);
+
+    }, [time, inputTime, isRunning]);
+
+    return (
+        <>
+            <div>{time}</div>
+            Count down from <input type="number" defaultValue={inputTime} disabled={inputIsDisabled} onChange={e => {
+                setInputTime(parseInt(e.target.value));
+                setTime(parseInt(e.target.value));
+            }} />
+            <Button value="Start" disabledValue={!isComplete || (time != inputTime)} inputTime={inputTime} onClick={handleClick} />
+            <Button value="Pause" disabledValue={!isRunning} inputTime={inputTime} onClick={handleClick} />
+            <Button value="Resume" disabledValue={(isRunning || isComplete || (time === 0))} inputTime={inputTime} onClick={handleClick} />
+            <Button value="Fast Forward" disabledValue={isComplete || (time === 0)} inputTime={inputTime} onClick={handleClick} />
+            <Button value="Reset" disabledValue={isComplete && (time === inputTime)} inputTime={inputTime} onClick={handleClick} />
+        </>
+    );
+};
 
 export default Countdown;
--- src/components/timers/Stopwatch.js
+++ src/components/timers/Stopwatch.js
@@ -6,26 +6,27 @@ const Stopwatch = () => {
     const [time, setTime] = useState(0);
     const [inputTime, setInputTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
-    const [isPaused, setIsPaused] = useState(false);
     const [isComplete, setIsComplete] = useState(true);
+    const [inputIsDisabled, setInputIsDisabled] = useState(false);
 
     const handleClick = value => {
         if (value === "Start") {
             setIsRunning(true);
             setIsComplete(false);
+            setInputIsDisabled(true);
         } else if (value === "Pause") {
             setIsRunning(false);
-            setIsPaused(true);
         } else if (value === "Resume") {
             setIsRunning(true);
-            setIsPaused(false);
         } else if (value === 'Fast Forward') {
             setTime(inputTime);
             setIsComplete(true);
+            setInputIsDisabled(false);
         } else if (value === 'Reset') {
             setTime(0);
             setIsComplete(true);
             setIsRunning(false);
+            setInputIsDisabled(false);
         }
     }
 
@@ -50,7 +51,7 @@ const Stopwatch = () => {
     return (
         <>
             <div>{time}</div>
-            Count to <input type="number" defaultValue={inputTime} onChange={e => {setInputTime(parseInt(e.target.value));}} /> seconds
+            Count to <input type="number" defaultValue={inputTime} disabled={inputIsDisabled} onChange={e => {setInputTime(parseInt(e.target.value));}} />
             <Button value="Start" disabledValue={!isComplete || (time === inputTime)} inputTime={inputTime} onClick={handleClick} />
             <Button value="Pause" disabledValue={!isRunning || (time === inputTime)} inputTime={inputTime} onClick={handleClick} />
             <Button value="Resume" disabledValue={(isRunning || isComplete || (time === inputTime))} inputTime={inputTime} onClick={handleClick} />
